0c9e2797c0078df99bf20a7f21bee0e0918febfb
Carl Saginsin <saguinsincarl8@gmail.com>
2023-11-26 20:26:49 +0700

    26/11/2023 8:30 pm updates

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import '../Style.css'
+import Graphic_Design_2 from '../assets/Graphic/Graphic_Design_2.png'
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
+    <>
+      <section className='Section'>
+            <div className="container">
+                <div className="left">
+                  <h1 className='About'>About Us</h1>
+                  <h1 className='Second_Title'>Welcome to BuildMastersHub,
+                      <br />the innovative web app 
+                      connecting construction 
+                      professionals with clients! 
+                  </h1>
+                  <p className='text'>
+                  At BuildMasterHub, we understand the importance of showcasing talent in the construction industry. Whether you're a seasoned professional with years of experience or a skilled artisan looking to expand your network, our platform offers a seamless solution to exhibit your skills and expertise. <br /> 
+                  <br ></br>
+                  Our mission is simple: to bridge the gap between talented construction professionals and clients seeking their expertise. By creating a user-friendly and interactive space, we aim to foster meaningful connections that benefit both parties.
+                  </p>
+                </div>
+                <div className="right">
+                  <img src={Graphic_Design_2} alt="Graphic Design" className='Graphic'/>
+                </div>
+            </div>
+      </section>
+
+      <section style={{ backgroundColor: '#FDF0CE', marginLeft: '5rem', marginRight: '5rem'}}>
+            <h1 style={{ textAlign: 'center', fontFamily: 'Chakra Petch', color: '#1854EE' }}>Why use this app</h1>
+            <h1 className='puncline'>Our Strategy is very personalized <br></br>
+                        to meet your needs construction <br></br> needs
+            </h1>
+      </section>
+    </>
   )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import '../Style.css'
+import { Container } from 'postcss'
+import Graphic_Design from '../assets/Graphic/Graphic_Design.png'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+<section className='Section'>
+      <div className="container">
+          <div className="left">
+            <h1 className='Title'>BUILDMASTERSHUB</h1>
+            <p className='text'>BuildMastersHub is more than just a digital platform; it's a dynamic web application <br /> designed to revolutionize the construction industry. At its core, this innovative project <br /> seeks to empower skilled construction workers and foremen by providing them with a robust <br /> space to not only exhibit their expertise but also forge meaningful connections with prospective <br /> clients.</p>
+            <button class="w-40 h-12 bg-white cursor-pointer rounded-3xl border-2 border-[#9748FF] shadow-[inset_0px_-2px_0px_1px_#9748FF] group hover:bg-[#9748FF] transition duration-300 ease-in-out">
+              <span class="font-medium text-[#333] group-hover:text-white">Visit Now</span>
+            </button>
+          </div>
+          <div className="right">
+            <img src={Graphic_Design} alt="Graphic Design" className='Graphic'/>
+          </div>
+      </div>
+</section>
+    </>
   )
 }
 
