b8a00d22a8d345e98c5b421cc8752b06f660b2ae
Carl Saginsin <saguinsincarl8@gmail.com>
2023-12-20 17:37:30 +0700

    20/12/2023 5:40 pm updates

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -160,12 +160,12 @@ const Profile = () => {
               class="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
             ></textarea>
           </div>
-            <button
-              type="submit"
-              class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800"
-            >
-              Post comment
-            </button>
+          <button
+            type="submit"
+            class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-500 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-600"
+          >
+            Post comment
+          </button>
           </form>
         </div>
       <Link to='/build-masters-hub'>
