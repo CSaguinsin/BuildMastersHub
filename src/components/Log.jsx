dbdab889d14dd70e9baa2de02c607d4c725d9fe6
Carl Saginsin <saguinsincarl8@gmail.com>
2024-01-02 06:52:55 +0700

    02/01/2024 6:55 am updates
    trying to fix the create account with google.

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -149,23 +149,24 @@ const Log = () => {
             </div>
             {/* Social login buttons */}
             <a
-                className="mb-3 flex w-full items-center justify-center rounded bg-primary px-7 pb-2.5 pt-3 text-center text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-                style={{ backgroundColor: "#7DC25E" }}
-                href="#!"
-                role="button"
-                data-te-ripple-init=""
-                data-te-ripple-color="light"
-                // onClick={handleGoogleSignIn} // Attach onClick event
-              >
-                {/* Google Icon */}
-                <img
-                  src={Google}
-                  className="mr-2 h-3.5 w-3.5"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                />
-                Sign in with Google
-              </a>
+              className="mb-3 flex w-full items-center justify-center rounded bg-primary px-7 pb-2.5 pt-3 text-center text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+              style={{ backgroundColor: "#7DC25E" }}
+              href="#!"
+              role="button"
+              data-te-ripple-init=""
+              data-te-ripple-color="light"
+              onClick={signInWithGoogle} // Update to signInWithGoogle
+            >
+              {/* Google Icon */}
+              <img
+                src={Google}
+                className="mr-2 h-3.5 w-3.5"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              />
+              Create account using Google
+            </a>
+
 
           </form>
         </div>
