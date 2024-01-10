94dc5b5218eddd74350f1f9115daa36fa1968688
Carl Saginsin <saguinsincarl8@gmail.com>
2024-01-10 13:01:23 +0700

    10/01/2024 1:05 pm updates

--- src/components/WhyUseThis.jsx
+++ src/components/WhyUseThis.jsx
@@ -1,5 +1,6 @@
 import { CloudArrowUpIcon, LockClosedIcon, ArrowPathIcon } from '@heroicons/react/20/solid'
 import Screenshot from '../assets/Graphic/screenshot.png'
+import screen from '../assets/Graphic/screen.png'
 
 export default function WhyUseThis() {
   return (
@@ -41,7 +42,7 @@ export default function WhyUseThis() {
         <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
           <img
             className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
-            src={Screenshot}
+            src={screen}
             alt=""
           />
         </div>
